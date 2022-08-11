5fdea6a6a266d65f23d6fa0ec98c6dc01776f6b1
step305 <step305@mail.ru>
2022-08-11 16:34:40 +0300

    0.4 max size of notepad requested from user at the start of program new decomposition added new errors processing and some new info messages

--- main.go
+++ main.go
@@ -8,19 +8,24 @@ import (
 	"strings"
 )
 
-const maxNotePadSize int = 5
 const numCommands int = 4
 const listPattern string = "[Info] %d: %s\n"
+const commandPromptRequest string = "Enter a command and data: > "
+const maxSizeRequest string = "Enter the maximum number of notes: > "
 
-// Errors
+// Errors and info messages
 const errorNotePadFull string = "[Error] Notepad is full"
+const errorUnknownCommand string = "[Error] Unknown command"
+const errorEmptyNote string = "[Error] Missing note argument"
+const errorInvalidInput string = "[Error] Invalid input while getting max notepad size"
+const infoNotePadEmpty string = "[Info] Notepad is empty"
 
 var quit bool = false
-var commandPrompt string = "Enter a command and data: > "
+var maxNotePadSize int = 5
 var notePad []string = make([]string, 0, maxNotePadSize)
 
-// Known commands
-var commands = [numCommands]string{
+// Known knownCommands
+var knownCommands = [numCommands]string{
 	"exit",
 	"create",
 	"list",
@@ -56,6 +61,9 @@ func actionOnCreate(newNote ...string) error {
 		if len(notePad) == maxNotePadSize {
 			return errors.New(errorNotePadFull)
 		}
+		if len(strings.TrimSpace(newNote[0])) == 0 {
+			return errors.New(errorEmptyNote)
+		}
 		notePad = append(notePad, newNote[0])
 	}
 	return nil
@@ -72,6 +80,10 @@ func actionOnClear(fakeArgs ...string) error {
 // action on command "list"
 func actionOnList(fakeArgs ...string) error {
 	_ = fakeArgs
+	if len(notePad) == 0 {
+		fmt.Println(infoNotePadEmpty)
+		return nil
+	}
 	for i, note := range notePad {
 		if len(note) > 0 {
 			fmt.Printf(listPattern, i+1, note)
@@ -80,42 +92,80 @@ func actionOnList(fakeArgs ...string) error {
 	return nil
 }
 
-func main() {
-	var line string
-	var input []string
-	var command string
-	var data string
+func getMaxNotePadSize() (int, error) {
+	var newSize int
+	fmt.Print(maxSizeRequest)
+	_, err := fmt.Scanln(&newSize)
+	if err != nil {
+		return 0, err
+	}
+	return newSize, nil
+}
+
+func getUserInput(scanner *bufio.Scanner) (command string, data string) {
+	line := scanner.Text()
+	input := strings.Split(line, " ")
+	command = input[0]
+	data = ""
+	if len(input) > 1 {
+		data = strings.Join(input[1:], " ")
+	}
+	return command, data
+}
+
+func createScanner() *bufio.Scanner {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanLines)
+	return scanner
+}
 
-	fmt.Printf(commandPrompt)
+func printCommandPrompt() {
+	fmt.Print(commandPromptRequest)
+}
+
+func findCommand(command string) (int, bool) {
+	for i, knownCommand := range knownCommands {
+		if knownCommand == command {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+func executeCommand(i int, data string) {
+	err := actions[i](data)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Print(commandConfirmation[i])
+	}
+}
+
+func main() {
+	var command string
+	var data string
+	var scanner *bufio.Scanner
+	var err error
+
+	scanner = createScanner()
+	maxNotePadSize, err = getMaxNotePadSize()
+	if err != nil || maxNotePadSize <= 0 {
+		fmt.Println(errorInvalidInput)
+		return
+	}
+
+	printCommandPrompt()
 	for scanner.Scan() {
-		line = scanner.Text()
-		input = strings.Split(line, " ")
-		command = input[0]
-		data = ""
-		if len(input) > 1 {
-			data = strings.Join(input[1:], " ")
-		}
-
-		foundCommand := false
-		for i, knownCommand := range commands {
-			if command == knownCommand {
-				err := actions[i](data)
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					fmt.Println(commandConfirmation[i])
-				}
-				foundCommand = true
-			}
-		}
-		if !foundCommand {
-			fmt.Println(command, data)
+		command, data = getUserInput(scanner)
+		commandIndex, found := findCommand(command)
+		if found {
+			executeCommand(commandIndex, data)
+		} else {
+			fmt.Println(errorUnknownCommand)
 		}
 		if quit {
 			break
 		}
-		fmt.Printf(commandPrompt)
+		printCommandPrompt()
 	}
 }
